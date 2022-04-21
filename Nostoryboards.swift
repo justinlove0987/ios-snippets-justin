a1c5c8f009cc63e25a499441869fbde4b6a9386e
justinlove0987 <71877118+justinlove0987@users.noreply.github.com>
2022-04-21 17:47:41 +0800

    No storyboards (use this in AppDelgate)

---
+++ Nostoryboards.swift
@@ -0,0 +1,11 @@
+var window: UIWindow?
+ 
+func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+ 
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.makeKeyAndVisible()
+    window?.backgroundColor = .systemBackground
+    window?.rootViewController = ViewController()
+ 
+    return true
+}
